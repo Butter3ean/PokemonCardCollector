62256d6db7e8bf0f7b4be3f7351aa99aed576ef4
Butter3ean <dhughes11438@gmail.com>
2022-09-27 13:57:51 -0400

    Added the entity card.kt and the Dao CardDao.kt

--- app/src/main/java/com/example/pokemoncardcollector/dao/CardDao.kt
+++ app/src/main/java/com/example/pokemoncardcollector/dao/CardDao.kt
@@ -1,4 +1,21 @@
 package com.example.pokemoncardcollector.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.pokemoncardcollector.entities.Card
+
+@Dao
 interface CardDao {
+
+    @Query("SELECT * FROM card")
+    fun getAll(): List<Card>
+
+    @Insert
+    fun insertAll(card: Card)
+
+    @Delete
+    fun delete(card: Card)
 }
+
--- app/src/main/java/com/example/pokemoncardcollector/entities/Card.kt
+++ app/src/main/java/com/example/pokemoncardcollector/entities/Card.kt
@@ -1,4 +1,36 @@
 package com.example.pokemoncardcollector.entities
 
-class Card {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Card(
+
+    @PrimaryKey val id: String,
+                val name: String,
+                val superType: String,
+                val subType: List<String>,
+                val level: String,
+                val hp: String,
+                val types: List<String>,
+                val evolvesFrom: List<String>,
+                val evolvesTo: List<String>,
+                val rules: List<String>,
+                val ancientTrait: HashMap<String, String>,
+                val abilities: List<HashMap<String, String>>,
+                val attacks: List<HashMap<String, String>>,
+                val weaknesses: List<HashMap<String, String>>,
+                val resistances: List<HashMap<String, String>>,
+                val retreatCost: List<String>,
+                val convertedRetreatCost: Int,
+                val set: HashMap<String, String>,
+                val number: String,
+                val artist: String,
+                val rarity: String,
+                val flavorText: String,
+                val nationalPokedexNumber: List<Int>,
+                val regulationMark: String,
+                val images: String,
+                val tcgplayer: String,
+
+)
