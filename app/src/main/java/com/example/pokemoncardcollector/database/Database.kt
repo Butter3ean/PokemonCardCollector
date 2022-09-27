14a7e8920f34d6ce8b18f466155e5fdd2a612060
Time2k18 <tm21909@georgiasouthern.edu>
2022-09-27 14:05:46 -0400

    Database.kt created and ready?

--- app/src/main/java/com/example/pokemoncardcollector/database/Database.kt
+++ app/src/main/java/com/example/pokemoncardcollector/database/Database.kt
@@ -1,4 +1,14 @@
 package com.example.pokemoncardcollector.database
 
-class Database {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.pokemoncardcollector.dao.CardDao
+import com.example.pokemoncardcollector.dao.SetDao
+import com.example.pokemoncardcollector.entities.Card
+import com.example.pokemoncardcollector.entities.Set
+
+@Database(entities = [Card::class, Set::class], version = 1)
+abstract class Database: RoomDatabase(){
+    abstract fun setDoa(): SetDao
+    abstract fun cardDoa(): CardDao
 }
